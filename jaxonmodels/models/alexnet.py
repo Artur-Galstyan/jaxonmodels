ab68c6478b211275f2d458e7fd7d6f368db8ad00
Artur Galstyan <mail@arturgalstyan.dev>
2025-03-04 21:16:06 +0100

    alexnet weights start

--- jaxonmodels/models/alexnet.py
+++ jaxonmodels/models/alexnet.py
@@ -1,7 +1,11 @@
+import os
+from urllib.request import urlretrieve
+
 import equinox as eqx
 import jax
 import jax.numpy as jnp
 import jaxtyping as jt
+from loguru import logger
 
 from jaxonmodels.layers.local_response_normalisation import LocalResponseNormalization
 
@@ -26,26 +30,26 @@ class AlexNet(eqx.Module):
 
     final: eqx.nn.Linear
 
-    def __init__(self, *, key: jt.PRNGKeyArray):
+    def __init__(self, *, n_classes: int, key: jt.PRNGKeyArray):
         _, *subkeys = jax.random.split(key, 10)
         self.conv1 = eqx.nn.Conv2d(
             in_channels=3,
-            out_channels=96,
+            out_channels=64,
             kernel_size=11,
             stride=4,
             key=subkeys[0],
         )
         self.conv2 = eqx.nn.Conv2d(
-            in_channels=96, out_channels=256, kernel_size=5, stride=1, key=subkeys[1]
+            in_channels=64, out_channels=192, kernel_size=5, stride=1, key=subkeys[1]
         )
         self.conv3 = eqx.nn.Conv2d(
-            in_channels=256, out_channels=384, kernel_size=3, stride=1, key=subkeys[2]
+            in_channels=192, out_channels=384, kernel_size=3, stride=1, key=subkeys[2]
         )
         self.conv4 = eqx.nn.Conv2d(
-            in_channels=384, out_channels=384, kernel_size=3, stride=1, key=subkeys[3]
+            in_channels=384, out_channels=256, kernel_size=3, stride=1, key=subkeys[3]
         )
         self.conv5 = eqx.nn.Conv2d(
-            in_channels=384, out_channels=256, kernel_size=3, stride=1, key=subkeys[4]
+            in_channels=256, out_channels=256, kernel_size=3, stride=1, key=subkeys[4]
         )
 
         self.lrn1 = LocalResponseNormalization()
@@ -54,13 +58,15 @@ class AlexNet(eqx.Module):
         self.max_pool_2 = eqx.nn.MaxPool2d(kernel_size=3, stride=2)
         self.max_pool_3 = eqx.nn.MaxPool2d(kernel_size=3, stride=2)
 
-        self.dense1 = eqx.nn.Linear(in_features=256, out_features=4096, key=subkeys[5])
+        self.dense1 = eqx.nn.Linear(in_features=9216, out_features=4096, key=subkeys[5])
         self.dense2 = eqx.nn.Linear(in_features=4096, out_features=4096, key=subkeys[6])
 
         self.dropout1 = eqx.nn.Dropout()
         self.dropout2 = eqx.nn.Dropout()
 
-        self.final = eqx.nn.Linear(in_features=4096, out_features=1, key=subkeys[7])
+        self.final = eqx.nn.Linear(
+            in_features=4096, out_features=n_classes, key=subkeys[7]
+        )
 
     def __call__(
         self,
@@ -103,3 +109,33 @@ class AlexNet(eqx.Module):
 
         x = self.final(x)
         return x
+
+    @staticmethod
+    def with_weights() -> "AlexNet":
+        alexnet = AlexNet(n_classes=1000, key=jax.random.key(0))
+
+        # from pytorch
+        weights_url = "https://download.pytorch.org/models/alexnet-owt-7be5be79.pth"
+
+        weights_dir = os.path.expanduser("~/.jaxonmodels/weights")
+        os.makedirs(weights_dir, exist_ok=True)
+
+        weights_file = os.path.join(weights_dir, "alexnet-owt-7be5be79.pth")
+        if not os.path.exists(weights_file):
+            logger.info(f"Downloading AlexNet weights to {weights_file}...")
+            urlretrieve(weights_url, weights_file)
+            logger.info("Download complete.")
+        else:
+            logger.info(f"AlexNet weights already exist at {weights_file}.")
+
+        import torch
+
+        weights_dict = torch.load(
+            weights_file, map_location=torch.device("cpu"), weights_only=True
+        )
+
+        print(weights_dict.keys())
+        for k in weights_dict.keys():
+            print(k, weights_dict[k].shape)
+
+        return alexnet
--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-import json
+from jaxonmodels.models.alexnet import AlexNet
 
-import jax
-from jaxonmodels.transformers.llama.llama3 import LLaMA
-from jaxonmodels.transformers.llama.model_args import LLaMAModelArgs
-
-
-with open("params.json", "r") as f:
-    params = json.load(f)
-
-model_args = LLaMAModelArgs(**params)
-key = jax.random.key(2)
-
-model = LLaMA(model_args, key=key)
+a = AlexNet.with_weights()
