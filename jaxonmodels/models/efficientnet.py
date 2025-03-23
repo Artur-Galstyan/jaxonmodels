f316ee1cfb917190614a9c0052c3c549ad54e22d
Artur Galstyan <mail@arturgalstyan.dev>
2025-03-23 13:42:07 +0100

    baby steps

--- jaxonmodels/models/efficientnet.py
+++ jaxonmodels/models/efficientnet.py
@@ -1,8 +1,10 @@
+import copy
 import math
 from dataclasses import dataclass
 
 import equinox as eqx
 import jax
+import jax.numpy as jnp
 from beartype.typing import Callable, Sequence
 from jaxtyping import Array, Float, PRNGKeyArray
 
@@ -34,7 +36,7 @@ class _MBConvConfig:
     input_channels: int
     out_channels: int
     num_layers: int
-    block: Callable[..., eqx.Module]
+    block: Callable[..., "MBConv | FusedMBConv"]
 
     @staticmethod
     def adjust_channels(
@@ -56,13 +58,10 @@ class MBConvConfig(_MBConvConfig):
         num_layers: int,
         width_mult: float = 1.0,
         depth_mult: float = 1.0,
-        block: Callable[..., eqx.Module] | None = None,
     ) -> None:
         input_channels = self.adjust_channels(input_channels, width_mult)
         out_channels = self.adjust_channels(out_channels, width_mult)
         num_layers = self.adjust_depth(num_layers, depth_mult)
-        if block is None:
-            block = MBConv
         super().__init__(
             expand_ratio,
             kernel,
@@ -70,7 +69,7 @@ class MBConvConfig(_MBConvConfig):
             input_channels,
             out_channels,
             num_layers,
-            block,
+            MBConv,
         )
 
     @staticmethod
@@ -88,10 +87,7 @@ class FusedMBConvConfig(_MBConvConfig):
         input_channels: int,
         out_channels: int,
         num_layers: int,
-        block: Callable[..., eqx.Module] | None = None,
     ) -> None:
-        if block is None:
-            block = FusedMBConv
         super().__init__(
             expand_ratio,
             kernel,
@@ -99,7 +95,7 @@ class FusedMBConvConfig(_MBConvConfig):
             input_channels,
             out_channels,
             num_layers,
-            block,
+            FusedMBConv,
         )
 
 
@@ -167,7 +163,11 @@ class Conv2dNormActivation(eqx.Module):
             self.activation = eqx.nn.Lambda(activation_layer)
 
     def __call__(
-        self, x: Float[Array, "c h w"], state: eqx.nn.State, inference: bool = False
+        self,
+        x: Float[Array, "c h w"],
+        state: eqx.nn.State,
+        inference: bool,
+        key: PRNGKeyArray | None = None,
     ) -> tuple[Float[Array, "c_out h_out w_out"], eqx.nn.State]:
         x = self.conv2d(x)
 
@@ -316,11 +316,113 @@ class MBConv(eqx.Module):
 
 
 class FusedMBConv(eqx.Module):
-    pass
+    fused_expand: Conv2dNormActivation
+    project: Conv2dNormActivation | None
+    stochastic_depth: StochasticDepth
+
+    use_res_connect: bool = eqx.field(static=True)
+
+    def __init__(
+        self,
+        cnf: FusedMBConvConfig,
+        stochastic_depth_prob: float,
+        key: PRNGKeyArray,
+    ):
+        if not (1 <= cnf.stride <= 2):
+            raise ValueError("illegal stride value")
+
+        self.use_res_connect = (
+            cnf.stride == 1 and cnf.input_channels == cnf.out_channels
+        )
+
+        expanded_channels = cnf.adjust_channels(cnf.input_channels, cnf.expand_ratio)
+        if expanded_channels != cnf.input_channels:
+            # fused expand
+            key, subkey, subkey2 = jax.random.split(key, 3)
+            self.fused_expand = Conv2dNormActivation(
+                cnf.input_channels,
+                expanded_channels,
+                kernel_size=cnf.kernel,
+                stride=cnf.stride,
+                norm_layer=BatchNorm,
+                activation_layer=jax.nn.silu,
+                key=subkey,
+            )
+
+            # project
+            self.project = Conv2dNormActivation(
+                expanded_channels,
+                cnf.out_channels,
+                kernel_size=1,
+                norm_layer=BatchNorm,
+                activation_layer=None,
+                key=subkey2,
+            )
+        else:
+            key, subkey = jax.random.split(key)
+            self.fused_expand = Conv2dNormActivation(
+                cnf.input_channels,
+                cnf.out_channels,
+                kernel_size=cnf.kernel,
+                stride=cnf.stride,
+                norm_layer=BatchNorm,
+                activation_layer=jax.nn.silu,
+                key=subkey,
+            )
+            self.project = None
+
+        self.stochastic_depth = StochasticDepth(stochastic_depth_prob, "row")
+
+    def __call__(
+        self, x: Array, state: eqx.nn.State, inference: bool, key: PRNGKeyArray
+    ) -> tuple[Array, eqx.nn.State]:
+        result, state = self.fused_expand(x, state, inference)
+
+        if self.project:
+            result, state = self.project(result, state, inference)
+
+        if self.use_res_connect:
+            result = self.stochastic_depth(result, inference, key)
+            result += x
+        return result, state
+
+
+class InvertedResidualBlock(eqx.Module):
+    layers: list[MBConv | FusedMBConv]
+
+    def __init__(self, layers: list[MBConv | FusedMBConv]):
+        self.layers = layers
+
+    def __call__(
+        self, x: Array, state: eqx.nn.State, inference: bool, key: PRNGKeyArray
+    ) -> tuple[Array, eqx.nn.State]:
+        for stage in self.layers:
+            key, subkey = jax.random.split(key)
+            x, state = stage(x, state, inference, subkey)
+
+        return x, state
+
+
+class Classifier(eqx.Module):
+    dropout: eqx.nn.Dropout
+    linear: eqx.nn.Linear
+
+    def __init__(
+        self, p: float, in_features: int, out_features: int, key: PRNGKeyArray
+    ):
+        self.dropout = eqx.nn.Dropout(p=p)
+        self.linear = eqx.nn.Linear(in_features, out_features, key=key)
+
+    def __call__(self, x: Array, inference: bool, key: PRNGKeyArray) -> Array:
+        x = self.dropout(x, inference=inference, key=key)
+        x = self.linear(x)
+
+        return x
 
 
 class EfficientNet(eqx.Module):
-    pass
+    features: list[Conv2dNormActivation | MBConv | FusedMBConv | InvertedResidualBlock]
+    avgpool: eqx.nn.AdaptiveAvgPool2d
 
     def __init__(
         self,
@@ -329,5 +431,91 @@ class EfficientNet(eqx.Module):
         stochastic_depth_prob: float = 0.2,
         num_classes: int = 1000,
         last_channel: int | None = None,
+        *,
+        key: PRNGKeyArray,
     ):
-        pass
+        if not inverted_residual_setting:
+            raise ValueError("The inverted_residual_setting should not be empty")
+        elif not (
+            isinstance(inverted_residual_setting, Sequence)
+            and all([isinstance(s, _MBConvConfig) for s in inverted_residual_setting])
+        ):
+            raise TypeError(
+                "The inverted_residual_setting should be List[MBConvConfig]"
+            )
+
+        key, *subkeys = jax.random.split(key, 10)
+
+        firstconv_output_channels = inverted_residual_setting[0].input_channels
+        self.features = []
+        self.features.append(
+            Conv2dNormActivation(
+                3,
+                firstconv_output_channels,
+                kernel_size=3,
+                stride=2,
+                norm_layer=BatchNorm,
+                activation_layer=jax.nn.silu,
+                key=subkeys[0],
+            )
+        )
+
+        # building inverted residual blocks
+        total_stage_blocks = sum(cnf.num_layers for cnf in inverted_residual_setting)
+        stage_block_id = 0
+        for cnf in inverted_residual_setting:
+            stage: list[MBConv | FusedMBConv] = []
+            for _ in range(cnf.num_layers):
+                block_cnf = copy.copy(cnf)
+
+                if stage:
+                    block_cnf.input_channels = block_cnf.out_channels
+                    block_cnf.stride = 1
+
+                sd_prob = (
+                    stochastic_depth_prob * float(stage_block_id) / total_stage_blocks
+                )
+                key, subkey = jax.random.split(key)
+                stage.append(block_cnf.block(block_cnf, sd_prob, BatchNorm, key=subkey))
+                stage_block_id += 1
+
+            self.features.append(InvertedResidualBlock(stage))
+
+        # building last several layers
+        lastconv_input_channels = inverted_residual_setting[-1].out_channels
+        lastconv_output_channels = (
+            last_channel if last_channel is not None else 4 * lastconv_input_channels
+        )
+        key, subkey = jax.random.split(key)
+        self.features.append(
+            Conv2dNormActivation(
+                lastconv_input_channels,
+                lastconv_output_channels,
+                kernel_size=1,
+                norm_layer=BatchNorm,
+                activation_layer=jax.nn.silu,
+                key=subkey,
+            )
+        )
+
+        self.avgpool = eqx.nn.AdaptiveAvgPool2d(1)
+        key, subkey = jax.random.split(key)
+        self.classifier = Classifier(
+            dropout, lastconv_output_channels, num_classes, subkey
+        )
+        # todo: kaiming init
+
+    def __call__(
+        self, x: Array, state: eqx.nn.State, inference: bool, key: PRNGKeyArray
+    ) -> tuple[Array, eqx.nn.State]:
+        for feature in self.features:
+            key, subkey = jax.random.split(key)
+            x, state = feature(x, state, inference, key)
+
+        x = self.avgpool(x)
+        x = jnp.ravel(x)
+
+        key, subkey = jax.random.split(key)
+        x = self.classifier(x, inference, key)
+
+        return x, state
