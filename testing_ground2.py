e4e9fcfce63918505e9e5ad8493cb2b4e8795766
Artur Galstyan <mail@arturgalstyan.dev>
2025-04-15 11:12:41 +0200

    removed some comments

--- testing_ground2.py
+++ testing_ground2.py
@@ -1,15 +1,33 @@
+import abc
+
+import equinox as eqx
 import jax
+from beartype.typing import Callable
+from jaxtyping import Array
 
 
-def fn(x, key):
-    return jax.random.normal(key=key) + x
+class CallableEqxModule(eqx.Module):
+    @abc.abstractmethod
+    def __call__(self, *_, **__):
+        pass
 
 
-n = 5
-key = jax.random.key(1)
-keys = jax.random.split(key, n)
-o1 = jax.vmap(fn, in_axes=(0, None))(jax.numpy.arange(n), key)
-print(o1)
+class BarNorm(CallableEqxModule):
+    x: Array
 
-o2 = jax.vmap(fn)(jax.numpy.arange(n), keys)
-print(o2)
+    def __call__(self, y):
+        return self.x + y
+
+
+class Foo(eqx.Module):
+    bar: CallableEqxModule
+
+    def __init__(self, bar: Callable[..., CallableEqxModule]):
+        self.bar = bar(1)
+
+    def __call__(self, x):
+        return self.bar(x) + x
+
+
+f = Foo(bar=BarNorm)
+print(f(jax.numpy.array(5)))
